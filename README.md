b0417ea788d924df2f97b0f737d8d2a34725f62f
David Vanjão <davidrafaeldj@hotmail.com>
2020-09-03 22:40:25 -0300

    Criacao da tabela codigo produtos

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # dv
 03-09 WORK: Criacao do arquivo teste.php para teste no codigo do produto.
+03-09 HOME: Criacao da nova tabela sql. O mesmo possui chave estrangeira.
--- aprendizado/modelos de verificacao de arquivo.php
+++ aprendizado/modelos de verificacao de arquivo.php
@@ -173,3 +173,80 @@ if (file_exists($arquivo_end)) {
     echo "O último arquivo utilizado foi: <strong>$name_file</strong> ";//. date ("d/m/Y", filectime($arquivo_end));
 }            
 ?>
+
+
+/*
+    $arquivo_glob = glob('arquivos/*.txt'); //Lista os arquivos dentro da pasta.
+    //$arquivo_glob = glob('//192.168.1.52/F/rel-david/*.txt'); //Lista os arquivos dentro da pasta.
+    
+    if(!empty($arquivo_glob)) {
+
+        $arquivo_end = end($arquivo_glob); //Pega o ultimo arquivo.        
+        $arquivo = file($arquivo_end); //Pega o arquivo e transforma em um array.
+        $arquivo = array_unique($arquivo);
+        //$arquivo = array_filter($arquivo, "impar"); // filtro para saber se o valor nao esta vazio.
+        //$arquivo = array_values(array_filter($arquivo, "trim"));
+        //array_values: retorna todos os valores de uma matrix.
+        //array_filter: Filtra elementos de uma matriz usando uma função de retorno de chamada.
+        //trim: tira os espaços em branco (ou outros caracteres) do início e do final de uma string.
+        //var_dump($arquivo);
+
+        foreach($arquivo as $linha) {
+            $end = explode(';', $linha);
+            $end_pos = end($end);
+            $array_end[$end[3]][] = end($end);
+            $lista = strrpos($linha, ';');
+            $nova_lista[$end[3]] = substr($linha, 0, $lista);
+        }
+        foreach ($array_end as $k => $v) {
+            if (array_key_exists($k, $nova_lista)) {
+                $list[] = $nova_lista[$k].implode(';', $v);
+            }
+            $implode = implode(';',$v).'<br>';
+            echo $k."<br>";
+        }
+        
+        var_dump($list);                         
+        
+        $name_file = basename($arquivo_end);
+        if (file_exists($arquivo_end)) {
+            echo "</br></br>O ultimo arquivo utilizado foi: <strong>$name_file</strong> ";//. date ("d/m/Y", filectime($arquivo_end));
+        } 
+    } else {
+        echo "NÃO EXISTE ARQUIVO PARA IMPORTAÇÃO.";
+    }
+    
+    
+ 
+# Agrega registros com colunas variáveis a um registro exclusivo
+ 
+$w = array('1;Lima;45;1','1;Lima;45;13','1;Vieira;45;51','1;Lima;45;32','1;Lima;45;74','1;Vieira;45;99','1;Lima;45;19');
+ 
+var_dump($w);
+ 
+$j = agregado($w);
+ 
+var_dump($j);
+ 
+# Função
+ 
+function agregado($a){
+    # Array temporária
+    $t = array();
+    foreach($a as $b)
+    {
+        $linha = explode(";", $b);
+        # Cada item será contado a partir do zero, vamos usar o Lima como key
+        $key = $linha[1]; // Segundo item como key
+        # Verifica se já está na nova array
+        if(!array_key_exists($key, $t))
+        {
+            # Array padrão (colocar cada coluna que sua string tiver)
+            $t[$key] = $linha[0] . ';' . $linha[1] . ';' . $linha[2] . ';' . $linha[3];
+        }else{
+            # Caso já exista, apenas adiciona o último item ao final da string
+            $t[$key] .= ';' . end($linha);
+        }
+    }
+    return $t;
+}*/
