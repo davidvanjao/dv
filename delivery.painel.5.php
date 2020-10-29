0423e22f90fa7b2c3ed6388f379f831cd0b185e0
David Work <davidrafaeldj@gmail.com>
2020-10-29 16:21:48 -0300

    data

--- delivery.painel.5.php
+++ delivery.painel.5.php
@@ -18,6 +18,27 @@ if($usuarios->temPermissao('USUARIO') == false) {
     exit;
 }
 
+$data = date('Y-m-d');
+
+$sql = "SELECT a.id, a.dataa, b.nome, c.cidadeEstado, c.logradouro, b.numero, a.statuss, c.regiao, DATE_FORMAT(a.dataa,'%d/%m/%Y') as saida_data
+            from tb_log_delivery a, tb_cliente b, tb_endereco c
+            WHERE a.dataa LIKE '$data'
+            and a.idCliente = b.id 
+            and b.idEndereco = c.id
+            order by a.statuss";
+
+
+if(isset($_GET['data']) && empty($_GET['data']) == false){
+
+    $data = addslashes($_GET['data']);
+
+    $sql = "SELECT a.id, a.dataa, b.nome, c.cidadeEstado, c.logradouro, b.numero, a.statuss, c.regiao, DATE_FORMAT(a.dataa,'%d/%m/%Y') as saida_data
+            from tb_log_delivery a, tb_cliente b, tb_endereco c
+            WHERE a.dataa LIKE '$data'
+            and a.idCliente = b.id 
+            and b.idEndereco = c.id
+            order by a.statuss";
+}
 
 ?>
 
@@ -57,8 +78,9 @@ if($usuarios->temPermissao('USUARIO') == false) {
 
                                 <div class="body-cesta">
                                     <div class="campo-inserir">
-
-                                        
+                                        <form class="busca-area" name="buscar" method="GET">
+                                            <input class="input-busca-delivery"type="date" value="<?php echo $data;?>" name="data" autocomplete="off" required="required" onchange="this.form.submit()"/>
+                                        </form>                                        
                                     </div>
 
                                     <div class="tabela-titulo">
@@ -79,50 +101,45 @@ if($usuarios->temPermissao('USUARIO') == false) {
                                     <div class="campo-listar">                
                                         <div class="tabela-lancamentos">
                                             <table>
-                                                <?php
+                                                <?php      
 
-                                                $sql = "SELECT a.id, a.dataa, b.nome, c.cidadeEstado, c.logradouro, b.numero, a.statuss, c.regiao, DATE_FORMAT(a.dataa,'%d/%m/%Y') as saida_data
-                                                from tb_log_delivery as a join tb_cliente as b join tb_endereco as c 
-                                                on a.idCliente = b.id 
-                                                and b.idEndereco = c.id
-                                                order by a.statuss";
+                                                    $sql = $pdo->query($sql);   
+                                                    if($sql->rowCount() > 0) {
+                                                        foreach($sql->fetchAll() as $delivery) {
 
-                                                $sql = $pdo->query($sql);   
-                                                if($sql->rowCount() > 0) {
-                                                    foreach($sql->fetchAll() as $delivery) {
+                                                            if($delivery['statuss']=="PEDIDO REALIZADO"){
+                                                                $cor="";
+                                                            }
+                                                            if($delivery['statuss']=="EM ANDAMENTO"){
+                                                                $cor="#ff0000";
+                                                            }
+                                                            if($delivery['statuss']=="LIBERADO PARA ENTREGA"){
+                                                                $cor="#ffa500";
+                                                            }
+                                                            if($delivery['statuss']=="SAIU PARA ENTREGA"){
+                                                                $cor="#008000";
+                                                            }  
 
-                                                        if($delivery['statuss']=="PEDIDO REALIZADO"){
-                                                            $cor="";
-                                                        }
-                                                        if($delivery['statuss']=="EM ANDAMENTO"){
-                                                            $cor="#ff0000";
-                                                        }
-                                                        if($delivery['statuss']=="LIBERADO PARA ENTREGA"){
-                                                            $cor="#ffa500";
-                                                        }
-                                                        if($delivery['statuss']=="SAIU PARA ENTREGA"){
-                                                            $cor="#008000";
-                                                        }  
-
-                                                        echo "<tr>";
-                                                        echo "<td style='width:5%;'>".$delivery['id']."</td>";
-                                                        echo "<td style='width:10%;'>".$delivery['saida_data']."</td>";
-                                                        echo "<td style='width:20%;'>".$delivery['nome']."</td>";
-                                                        echo "<td style='width:10%;'>".$delivery['cidadeEstado']."</td>";
-                                                        echo "<td style='width:20%;'>".$delivery['logradouro']."</td>";  
-                                                        echo "<td style='width:5%;'>".$delivery['numero']."</td>"; 
-                                                        echo "<td style='width:6%;'>".$delivery['regiao']."</td>";
+                                                            echo "<tr>";
+                                                            echo "<td style='width:5%;'>".$delivery['id']."</td>";
+                                                            echo "<td style='width:10%;'>".$delivery['saida_data']."</td>";
+                                                            echo "<td style='width:20%;'>".$delivery['nome']."</td>";
+                                                            echo "<td style='width:10%;'>".$delivery['cidadeEstado']."</td>";
+                                                            echo "<td style='width:20%;'>".$delivery['logradouro']."</td>";  
+                                                            echo "<td style='width:5%;'>".$delivery['numero']."</td>"; 
+                                                            echo "<td style='width:6%;'>".$delivery['regiao']."</td>";
 
 
-                                                        echo "<td style='width:10%; background-color:$cor;'>".$delivery['statuss']."</td>";                        
-                                                        echo "</tr>";  
+                                                            echo "<td style='width:10%; background-color:$cor;'>".$delivery['statuss']."</td>";                        
+                                                            echo "</tr>";  
 
-                                                    
-                                                    }
-                                                } else {
                                                         
-                                                        echo "Nenhum produto encontrado.";
+                                                        }
+                                                    } else {
+                                            
+                                                        echo "Nenhum lançamento encontrado!";
                                                     }
+                                                
                                                 ?>                                             
 
                                             </table>
